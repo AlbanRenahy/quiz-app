e32e7527dc7a141281bf618b09a40035adbaff79
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-05-01 10:49:24 +0200

    ADD: components structure + router

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { StyledProps } from "../../utils/interfaces";
+
+const Footer: React.FC<StyledProps> = ({ className }) => {
+  return <footer>&copy; Alban Renahy</footer>;
+};
+
+export default styled(Footer)``;
