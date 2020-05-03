6432341b25909bf59ab7f0cd1299878e3e96e509
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-05-03 07:06:20 +0200

    Add functionnality to answer the questions and record the answers

--- src/store/quiz/types.ts
+++ src/store/quiz/types.ts
@@ -1,45 +1,67 @@
 export interface Question {
-  category: string;
-  type: string;
-  difficulty: string;
-  question: string;
-  correct_answer: string;
-  incorrect_answers: [string];
-}
-
-export interface QuestionState {
-  questions: Question[];
-  difficulty: string;
-  score: number;
-  loading: boolean;
-}
-
-export const SET_QUESTIONS = "SET_QUESTIONS";
-export const FETCH_QUESTIONS = "FETCH_QUESTIONS";
-export const UPDATE_DIFFICULTY = "UPDATE_DIFFICULTY";
-export const UPDATE_SCORE = "UPDATE_SCORE";
-
-interface FetchQuestionsAction {
+    category: string;
+    type: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: [string];
+  }
+  
+  export interface QuestionState {
+    questions: Question[];
+    answers: boolean[];
+    difficulty: string;
+    score: number;
+    loading: boolean;
+    currentQuestion: number;
+    maxQuestions: number;
+  }
+  
+  export const SET_QUESTIONS = "SET_QUESTIONS";
+  export const FETCH_QUESTIONS = "FETCH_QUESTIONS";
+  export const CLEAR_QUESTIONS = "CLEAR_QUESTIONS";
+  export const INCREMENT_CURRENT = "INCREMENT_CURRENT";
+  export const UPDATE_DIFFICULTY = "UPDATE_DIFFICULTY";
+  export const UPDATE_SCORE = "UPDATE_SCORE";
+  export const ADD_ANSWER = "ADD_ANSWER";
+  
+  interface FetchQuestionsAction {
     type: typeof FETCH_QUESTIONS;
   }
-
-interface SetQuestionsAction {
-  type: typeof SET_QUESTIONS;
-  payload: Question[];
-}
-
-interface UpdateDifficultyAction {
-  type: typeof UPDATE_DIFFICULTY;
-  payload: string;
-}
-
-interface UpdateScoreAction {
-  type: typeof UPDATE_SCORE;
-  payload: number;
-}
-
-export type QuizActionTypes =
-  | SetQuestionsAction
-  | FetchQuestionsAction
-  | UpdateDifficultyAction
-  | UpdateScoreAction;
+  
+  interface SetQuestionsAction {
+    type: typeof SET_QUESTIONS;
+    payload: Question[];
+  }
+  
+  interface ClearQuestionsAction {
+    type: typeof CLEAR_QUESTIONS;
+  }
+  
+  interface IncrementCurrent {
+    type: typeof INCREMENT_CURRENT;
+  }
+  
+  interface UpdateDifficultyAction {
+    type: typeof UPDATE_DIFFICULTY;
+    payload: string;
+  }
+  
+  interface UpdateScoreAction {
+    type: typeof UPDATE_SCORE;
+    payload: number;
+  }
+  
+  interface AddAnswerAction {
+    type: typeof ADD_ANSWER;
+    payload: boolean;
+  }
+  
+  export type QuizActionTypes =
+    | SetQuestionsAction
+    | FetchQuestionsAction
+    | ClearQuestionsAction
+    | UpdateDifficultyAction
+    | UpdateScoreAction
+    | IncrementCurrent
+    | AddAnswerAction;
